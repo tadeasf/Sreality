16d73b33f82e735716f0dd23cd67a7ce9fdc5db1
tadeasf <taddy.fort@gmail.com>
2024-06-04 03:11:31 +0200

    init

--- db_managment/data_manager.py
+++ db_managment/data_manager.py
@@ -6,9 +6,10 @@ import re
 import sqlite3
 import os
 
-from config import Config 
+from config import Config
 
 from utils.logger import logger
+
 
 class DataManager:
     """
@@ -16,37 +17,38 @@ class DataManager:
     Inserting new estate, updating estate info in estate_detail table.
     Adding new prices to scraped_prices table.
     """
-    
-    def __init__(self) -> None: 
-        self.cf = Config()      
-        
+
+    def __init__(self) -> None:
+        self.cf = Config()
+
     def _get_connection(self):
-        
         try:
             conn = sqlite3.connect(f"{self.cf.project_path}/{self.cf.db_name}")
             return conn
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error connecting to database: {e}') 
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error connecting to database: {e}'
+            )
 
             return None
 
     def get_all_rows(self, table_name):
-        
-        conn = self._get_connection()        
+        conn = self._get_connection()
         try:
             query = f"SELECT * FROM {table_name}"
             df = pd.read_sql_query(query, conn)
             conn.close()
             return df
-        
+
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error loading rows from table {table_name}: {e}') 
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error loading rows from table {table_name}: {e}'
+            )
             conn.rollback()
-        
+
         conn.close()
-        
+
     def clear_table(self, table_name):
-        
         conn = self._get_connection()
         try:
             cursor = conn.cursor()
@@ -54,13 +56,14 @@ class DataManager:
             conn.commit()
             cursor.close()
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error clearing table {table_name}: {e}') 
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error clearing table {table_name}: {e}'
+            )
             conn.rollback()
-        
+
         conn.close()
-            
+
     def drop_table(self, table_name):
-        
         conn = self._get_connection()
         try:
             cursor = conn.cursor()
@@ -68,15 +71,16 @@ class DataManager:
             conn.commit()
             cursor.close()
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error dropping table {table_name}: {e}') 
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error dropping table {table_name}: {e}'
+            )
             conn.rollback()
-        
+
         conn.close()
-               
+
     def create_table(self, table_name):
-        
         conn = self._get_connection()
-        
+
         table = self.cf.table_definitions[table_name]
         try:
             cursor = conn.cursor()
@@ -84,73 +88,76 @@ class DataManager:
             conn.commit()
             cursor.close()
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error creating table {table_name}: {e}') 
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error creating table {table_name}: {e}'
+            )
             conn.rollback()
-        
+
         conn.close()
-                 
+
     def insert_new_estates(self, df):
-        
         conn = self._get_connection()
         try:
             cursor = conn.cursor()
-            
+
             data_to_upload = []
             for i in range(len(df)):
                 r = df.iloc[i]
-                data_to_upload.append([
-                    str(r['code']),
-                    str(r['description']),
-                    str(r['meta_description']),
-                    str(r['category_main_cb']),
-                    str(r['category_type_cb']),
-                    str(r['category_sub_cb']),
-                    str(r['broker_id']),
-                    str(r['broker_company']),
-                    str(r['furnished']),
-                    str(r['locality_gps_lat']),
-                    str(r['locality_gps_lon']),
-                    str(r['locality']), 
-                    str(r['city']), 
-                    str(r['district']), 
-                    str(r['region']),
-                    str(r['object_type']),
-                    str(r['parking_lots']),
-                    str(r['locality_street_id']),
-                    str(r['locality_district_id']),
-                    str(r['locality_ward_id']),
-                    str(r['locality_region_id']),
-                    str(r['locality_quarter_id']),
-                    str(r['locality_municipality_id']),
-                    str(r['locality_country_id']),
-                    str(r['terrace']),
-                    str(r['balcony']),
-                    str(r['loggia']),
-                    str(r['basin']),
-                    str(r['cellar']),
-                    str(r['building_type']),
-                    str(r['object_kind']),
-                    str(r['ownership']),
-                    str(r['low_energy']),
-                    str(r['easy_access']),
-                    str(r['building_condition']),
-                    str(r['garage']),
-                    str(r['room_count_cb']),
-                    str(r['energy_efficiency_rating_cb']),
-                    str(r['note_about_price']),
-                    str(r['id_of_order']),
-                    str(r['last_update']),
-                    str(r['material']),
-                    str(r['age_of_building']),
-                    str(r['ownership_type']),
-                    str(r['floor']),
-                    str(r['usable_area']),
-                    str(r['floor_area']),
-                    str(r['energy_efficiency_rating']),
-                    str(r['no_barriers']),
-                    str(r['start_of_offer']),
-                    str(r['timestamp']),
-                    ])
+                data_to_upload.append(
+                    [
+                        str(r["code"]),
+                        str(r["description"]),
+                        str(r["meta_description"]),
+                        str(r["category_main_cb"]),
+                        str(r["category_type_cb"]),
+                        str(r["category_sub_cb"]),
+                        str(r["broker_id"]),
+                        str(r["broker_company"]),
+                        str(r["furnished"]),
+                        str(r["locality_gps_lat"]),
+                        str(r["locality_gps_lon"]),
+                        str(r["locality"]),
+                        str(r["city"]),
+                        str(r["district"]),
+                        str(r["region"]),
+                        str(r["object_type"]),
+                        str(r["parking_lots"]),
+                        str(r["locality_street_id"]),
+                        str(r["locality_district_id"]),
+                        str(r["locality_ward_id"]),
+                        str(r["locality_region_id"]),
+                        str(r["locality_quarter_id"]),
+                        str(r["locality_municipality_id"]),
+                        str(r["locality_country_id"]),
+                        str(r["terrace"]),
+                        str(r["balcony"]),
+                        str(r["loggia"]),
+                        str(r["basin"]),
+                        str(r["cellar"]),
+                        str(r["building_type"]),
+                        str(r["object_kind"]),
+                        str(r["ownership"]),
+                        str(r["low_energy"]),
+                        str(r["easy_access"]),
+                        str(r["building_condition"]),
+                        str(r["garage"]),
+                        str(r["room_count_cb"]),
+                        str(r["energy_efficiency_rating_cb"]),
+                        str(r["note_about_price"]),
+                        str(r["id_of_order"]),
+                        str(r["last_update"]),
+                        str(r["material"]),
+                        str(r["age_of_building"]),
+                        str(r["ownership_type"]),
+                        str(r["floor"]),
+                        str(r["usable_area"]),
+                        str(r["floor_area"]),
+                        str(r["energy_efficiency_rating"]),
+                        str(r["no_barriers"]),
+                        str(r["start_of_offer"]),
+                        str(r["timestamp"]),
+                    ]
+                )
 
             query = f"""
                     INSERT INTO estate_detail (
@@ -173,64 +180,64 @@ class DataManager:
                             ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
                             ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
                             ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                    """             
+                    """
             cursor.executemany(query, data_to_upload)
             conn.commit()
 
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error inserting offer: {e}')        
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error inserting offer: {e}'
+            )
             conn.rollback()
-            
+
         conn.close()
-        
+
     # TODO: future use?
     def _update_estate(self, df):
         """
         Updates all estates from the DF. Currently only Timestamp, but could be any detail.
         """
-        
+
         conn = self._get_connection()
         try:
             cursor = conn.cursor()
-            
+
             data_to_upload = []
             for i in tqdm(range(len(df))):
                 r = df.iloc[i]
 
-                data_to_upload.append([
-                    r["timestamp"]
-                    ])
+                data_to_upload.append([r["timestamp"]])
 
             query = f"""
                     UPDATE estate_detail SET 
                         last_update = ?
                     WHERE code = ?
-                    """          
+                    """
             cursor.executemany(query, data_to_upload)
             conn.commit()
 
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error updating offer: {e}')        
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error updating offer: {e}'
+            )
             conn.rollback()
-            
+
         conn.close()
-    
+
     def insert_new_price(self, df):
-        
         conn = self._get_connection()
         try:
-            #? only here foreing keys constraint can be applied and need to be actively enforced
-            #conn.execute("PRAGMA foreign_keys = ON")
+            # ? only here foreing keys constraint can be applied and need to be actively enforced
+            # conn.execute("PRAGMA foreign_keys = ON")
             cursor = conn.cursor()
-            
+
             data_to_upload = []
             for i in range(len(df)):
                 r = df.iloc[i]
-                data_to_upload.append([str(r['estate_id']),
-                                       str(r["price"]),
-                                       str(r["crawled_at"])
-                                       ])
-                
+                data_to_upload.append(
+                    [str(r["estate_id"]), str(r["price"]), str(r["crawled_at"])]
+                )
+
             query = f"""
                     INSERT INTO price_history (
                     estate_id, price, crawled_at)
@@ -240,12 +247,9 @@ class DataManager:
             conn.commit()
 
         except sqlite3.Error as e:
-            logger.error(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error inserting offer: {e}')        
+            logger.error(
+                f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Error inserting offer: {e}'
+            )
             conn.rollback()
-            
+
         conn.close()
-   
-    
-
-    
-
--- run.py
+++ run.py
@@ -1,30 +1,28 @@
 import pandas as pd
 from typing import Optional
 import os
-
-os.chdir("c:\\Users\\jirka\\Documents\\MyProjects\\Sreality")
 from scraper.scraper import SrealityScraper
 from db_managment.data_manager import DataManager
 from utils.utils import Utilities, GeoData
 from utils.mailing import EmailService
 from config import Config
-
 from utils.logger import logger
 
+
 class Runner:
-    
-    def __init__(self) -> None: 
-        self.cf = Config()   
+    def __init__(self) -> None:
+        self.cf = Config()
         self.scraper = SrealityScraper()
         self.data_manager = DataManager()
         self.utils = Utilities()
         self.mailing = EmailService()
         self.geodata = GeoData()
-        
-    def scrape_prices_and_details(self,
-                    scrape_prodej_byty: Optional[bool] = True,
-                    scrape_all: Optional[bool] = True
-                    ) -> pd.DataFrame:
+
+    def scrape_prices_and_details(
+        self,
+        scrape_prodej_byty: Optional[bool] = True,
+        scrape_all: Optional[bool] = True,
+    ) -> pd.DataFrame:
         """
         1.) It runs scraping process for 'prodej_byty' and/or 'all' estates.
         2.) After scraping prices, there is a check of missing estate details in DB and JSON files,
@@ -32,132 +30,146 @@ class Runner:
         3.) For new estate_details files there is GeoData obtained and saved
         """
         full_datetime, date_to_save = self.utils.generate_timestamp()
-        logger.info(f'Starting scraping process.')
-        
+        logger.info(f"Starting scraping process.")
+
         # scrape all with filter
         if scrape_prodej_byty:
-            logger.info(f'Running scraper for Prodeje - Byty.')
-            data_prodej_byty = self.scraper.scrape_all_with_filter(timestamp=full_datetime, category_main_cb=1,category_type_cb=1)
+            logger.info(f"Running scraper for Prodeje - Byty.")
+            data_prodej_byty = self.scraper.scrape_all_with_filter(
+                timestamp=full_datetime, category_main_cb=1, category_type_cb=1
+            )
             df_data_prodej_byty = pd.DataFrame(data_prodej_byty)
             self.utils.safe_save_csv(df_data_prodej_byty, f"data_{date_to_save}")
-        
+
         # scrape basically all
         if scrape_all:
-            logger.info(f'Running scraper for All')
+            logger.info(f"Running scraper for All")
             data_all = self.scraper.scrape_all_with_filter(timestamp=full_datetime)
             df_data_all = pd.DataFrame(data_all)
             self.utils.safe_save_csv(df_data_all, f"data_all_{date_to_save}")
-        
-        #TODO: for scrape_prodej_byty, resp for scrape_all ..
-        # ? check existing codes in DB. 
+
+        # TODO: for scrape_prodej_byty, resp for scrape_all ..
+        # ? check existing codes in DB.
         if scrape_prodej_byty:
-            existing_codes = set(self.data_manager.get_all_rows("estate_detail")["code"])
+            existing_codes = set(
+                self.data_manager.get_all_rows("estate_detail")["code"]
+            )
             df_codes = set(df_data_prodej_byty["code"].unique())
-            
-            #df_missing = [x for x in df_codes if x not in list(existing_codes)]
+
+            # df_missing = [x for x in df_codes if x not in list(existing_codes)]
             df_missing = df_codes - existing_codes
-            
+
             # ? check existing codes in JSON - might be not processed previously
             df_missing = self.utils.compare_codes_to_existing_jsons(df_missing)
-            logger.info(f'There are {len(df_missing)} new codes to scrape')
+            logger.info(f"There are {len(df_missing)} new codes to scrape")
 
             # ? this handles the case when empty JSON would be created and not read by GeoData
             if len(df_missing) > 0:
                 self.scraper.scrape_specific_estates(df_missing, full_datetime)
-                logger.info(f'Finished scraping missing estate details')        
+                logger.info(f"Finished scraping missing estate details")
             else:
-                logger.info(f'No need to scrape missing estate details')             
-            
+                logger.info(f"No need to scrape missing estate details")
+
         # TODO: deduplicate this section, the only difference is data_all vs df_data_prodej_byty
         # TODO: and saving file name maybe?
         # TODO: or simply just run one function twice, based on argument there will be file_name.
         if scrape_all:
-            existing_codes = set(self.data_manager.get_all_rows("estate_detail")["code"])
+            existing_codes = set(
+                self.data_manager.get_all_rows("estate_detail")["code"]
+            )
             df_codes = set(df_data_all["code"].unique())
-            
+
             df_missing = df_codes - existing_codes
-            
+
             df_missing = self.utils.compare_codes_to_existing_jsons(df_missing)
-            logger.info(f'There are {len(df_missing)} new codes to scrape.')
+            logger.info(f"There are {len(df_missing)} new codes to scrape.")
 
             if len(df_missing) > 0:
                 self.scraper.scrape_specific_estates(df_missing, full_datetime)
-                logger.info(f'Finished scraping missing estate details for ALL')        
+                logger.info(f"Finished scraping missing estate details for ALL")
             else:
-                logger.info(f'No need to scrape missing estate details for ALL')        
+                logger.info(f"No need to scrape missing estate details for ALL")
 
-    #TODO: tím že je to oděělené od scrapingu, je nutné to mít jako samostatný run, nebo nechat jako util?
+    # TODO: tím že je to oděělené od scrapingu, je nutné to mít jako samostatný run, nebo nechat jako util?
     def update_geodata(self, list_of_jsons=None):
-        #? Get Geodata for all JSON files that do not have any, usually for new estate_detail scraped.
-        logger.info(f'Updating GeoData')
+        # ? Get Geodata for all JSON files that do not have any, usually for new estate_detail scraped.
+        logger.info(f"Updating GeoData")
         self.geodata.enrich_jsons_with_geodata(list_of_jsons=list_of_jsons)
-        logger.info(f'Updating GeoData Done')
-        
-    #TODO: pracovat pouze s NOVÝMI, nebo jinak označenými JSONy. Nikoliv merge all a pak porovnej?
+        logger.info(f"Updating GeoData Done")
+
+    # TODO: pracovat pouze s NOVÝMI, nebo jinak označenými JSONy. Nikoliv merge all a pak porovnej?
     def input_all_estates_to_db(self):
         """
         This process takes all existing JSONs with estate details, compare unique codes to those in database,
         and preprocess and insert the new ones into estate_detail table.
         """
-        
-        #? First prepare all existing JSONs to df, and compare to existing DB
-        logger.info(f'Starting processing JSONs to estate_detail table.')
+
+        # ? First prepare all existing JSONs to df, and compare to existing DB
+        logger.info(f"Starting processing JSONs to estate_detail table.")
         df_new = self.utils.prepare_estate_detail_jsons_to_df()
-        
+
         df = self.data_manager.get_all_rows("estate_detail")
-        logger.info(f'Already {len(df)} rows in estate_detail.')
-        
+        logger.info(f"Already {len(df)} rows in estate_detail.")
+
         existing_codes = df["code"].unique() if len(df) > 0 else []
         df_new = df_new[~df_new["code"].isin(existing_codes)]
-        logger.info(f'Prepared {len(df_new)} new rows.')
-                
-        #? Take offers which codes are not yet in DB and load them into DB
+        logger.info(f"Prepared {len(df_new)} new rows.")
+
+        # ? Take offers which codes are not yet in DB and load them into DB
         if len(df_new) > 0:
             self.data_manager.insert_new_estates(df_new)
-            logger.info(f'Insert into estate_detail DONE.')
+            logger.info(f"Insert into estate_detail DONE.")
         else:
-            logger.info(f'No need to insert into estate_detail. DONE.')
+            logger.info(f"No need to insert into estate_detail. DONE.")
 
     def input_all_prices_to_db(self):
         """
         This process takes all existing CSVs with estate prices, compare to existing rows in database,
         and preprocess and insert the new ones into price_history table.
         """
-        
-        #? Prepare all CSV with prices to df, and compare to existing DB
-        logger.info(f'Starting processing CSVs to price_history table.')
+
+        # ? Prepare all CSV with prices to df, and compare to existing DB
+        logger.info(f"Starting processing CSVs to price_history table.")
         df_new = self.utils.prepare_price_history_csv_to_df()
         df_new = df_new.drop_duplicates()
-        
-        df = self.data_manager.get_all_rows("price_history")
-        logger.info(f'Already {len(df)} rows in price_history.')
-        
-        #? cool way how to find rows which are not part of another df
-        df_new = df_new[~df_new.isin(df.to_dict(orient='list')).all(axis=1)]
-        logger.info(f'Prepared {len(df_new)} new rows.')
 
-        #? Take prices which are not yet in DB and load them into DB ??
+        df = self.data_manager.get_all_rows("price_history")
+        logger.info(f"Already {len(df)} rows in price_history.")
+
+        # ? cool way how to find rows which are not part of another df
+        df_new = df_new[~df_new.isin(df.to_dict(orient="list")).all(axis=1)]
+        logger.info(f"Prepared {len(df_new)} new rows.")
+
+        # ? Take prices which are not yet in DB and load them into DB ??
         if len(df_new) > 0:
             self.data_manager.insert_new_price(df_new)
-            logger.info(f'Inserting into price_history DONE.')
+            logger.info(f"Inserting into price_history DONE.")
         else:
-            logger.info(f'No need to insert into price_history. DONE.')
-    
-    #? Combination of scraping, GeoPandas, and Updating DB
-    def run_complete_scraping(self,
-                            scrape_prodej_byty: Optional[bool] = True,
-                            scrape_all: Optional[bool] = True)->pd.DataFrame:
+            logger.info(f"No need to insert into price_history. DONE.")
+
+    # ? Combination of scraping, GeoPandas, and Updating DB
+    def run_complete_scraping(
+        self,
+        scrape_prodej_byty: Optional[bool] = True,
+        scrape_all: Optional[bool] = True,
+    ) -> pd.DataFrame:
         """
         This is a combination of Scraping, Follo-up scraping, GeoPandas, and Updating both DB tables with new rows.
         """
-        logger.info(f'STARTING complete process of data gathering.')
+        logger.info(f"STARTING complete process of data gathering.")
 
-        self.scrape_prices_and_details(scrape_prodej_byty=scrape_prodej_byty,
-                                       scrape_all=scrape_all)
-        
+        self.scrape_prices_and_details(
+            scrape_prodej_byty=scrape_prodej_byty, scrape_all=scrape_all
+        )
+
         self.update_geodata(list_of_jsons=None)
-        
+
         self.input_all_estates_to_db()
         self.input_all_prices_to_db()
-        
-        logger.info(f'FINISHING complete process of data gathering.')
+
+        logger.info(f"FINISHING complete process of data gathering.")
+
+
+if __name__ == "__main__":
+    runner = Runner()
+    runner.run_complete_scraping(scrape_prodej_byty=True, scrape_all=True)
